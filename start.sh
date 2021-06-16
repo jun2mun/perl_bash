cb33ccf2af36e4ea61f1adc5c55d9f1094137561
jun <46515663+jun2mun@users.noreply.github.com>
2021-06-16 17:44:32 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 PATH=$PATH:/usr/bin:/usr/sbin:/bin:/sbin
 
-perl bin/automation_version3_1.pl
+perl bin/automation_sftp_upload.pl
 echo "complete\n"
-perl bin/automation_version3_2.pl
+perl bin/automation_ssh_command.pl
 echo "complete\n"
-perl bin/automation_version3_3.pl
+perl bin/automation_sftp_download.pl
 echo "complete\n"
